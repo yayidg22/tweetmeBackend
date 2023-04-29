ca8ec52e1850e800213a3c4cc3e8690ca05a8ee1
yayidg22 <yayidg2010@gmail.com>
2023-04-29 15:38:04 -0400

    added updateselectedcharacter route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -8,6 +8,7 @@ router.post("/signup", AuthController.register);
 router.post("/signin", AuthController.login);
 router.get("/getall", auth, AuthController.getAllUsers);
 router.get("/user", auth, AuthController.getUserData);
+router.get("/updateselectedcharacter", auth, AuthController.updateSelectedCharacter);
 
 export default router;
 
